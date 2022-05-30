f7bcb0667ce0a6a8e01d5ed67a1ba5a1a2d743bd
sweta <sweetysatasiya1610@gmail.com>
2022-05-30 21:20:19 +0530

    Remove Unnecessary File

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // const { response } = require("express");
 
-console.log("Client side java script File is Created");
+// console.log("Client side java script File is Created");
 fetch('https://puzzle.mead.io/puzzle').then((response)=>{
     response.json().then((data)=>{
         console.log(data);
